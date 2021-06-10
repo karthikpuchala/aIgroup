a1df15c71530313a877ae1a8b77b9bfaf86fdcfc
karthikpuchala <85655149+karthikpuchala@users.noreply.github.com>
2021-06-10 07:06:27 +0530

    created fibnocci.py
    souce code for fibnocci series

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+nterms=int(input("how many terms:"))
+n1=0
+n2=1
+if nterms<=0;
+  print("enter positive value:")
+elif nterms==1;
+  print("fibnocci upto nterms")
+  print(n1)
+  nth=n1+n2;  
+  n1=n2;
+  n2=nth;
+else:
+  print("not a fibnocci value:")
+  
+  
